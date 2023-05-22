bb6332a05ba625af6fdc0de7dfc83cec9b822478
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2023-05-22 22:25:40 +0200

    feat: add toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type AppType } from "next/app";
 import { Inter } from "next/font/google";
 import { api } from "@/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +12,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <ClerkProvider {...pageProps}>
             <div className={inter.className}>
                 <Component {...pageProps} />
+                <Toaster />
             </div>
         </ClerkProvider>
     );
--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,8 +1,10 @@
+import toast from "react-hot-toast";
+
 export default async function copyToClipboard(text: string): Promise<void> {
     try {
         await navigator.clipboard.writeText(text);
 
-        alert("Copied to clipboard!");
+        toast.success("Copied to clipboard!");
     } catch (error) {
         console.error("Failed to copy text to clipboard:", error);
     }
