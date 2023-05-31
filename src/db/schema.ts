13e977c66aa9e50094b55d0a6cac3ed9cef2949d
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2023-05-31 21:19:43 +0200

    refactor: createdAt default value

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,24 +7,22 @@ import {
     int,
 } from "drizzle-orm/mysql-core";
 
-const createdAt = timestamp("created_at").defaultNow().notNull();
-
 export const chatbots = mysqlTable("chatbots", {
     id: serial("id").primaryKey(),
     user: varchar("user_id", { length: 255 }).notNull(),
-    name: varchar("name", { length: 255 }).notNull(),
+    displayName: varchar("display_name", { length: 255 }).notNull(),
     systemMessage: varchar("system_message", { length: 1000 }).notNull(),
-    createdAt,
+    createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
 export const contentEntries = mysqlTable("content_entries", {
     id: serial("id").primaryKey(),
     chatbot: int("chatbot_id").notNull(),
-    createdAt,
+    createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
 export const pineconeIndexes = mysqlTable("pinecone_indexes", {
     id: serial("id").primaryKey(),
     content: text("content").notNull(),
-    createdAt,
+    createdAt: timestamp("created_at").defaultNow().notNull(),
 });
